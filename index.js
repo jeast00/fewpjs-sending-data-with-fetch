7543547e7b726cbd00adf131c5f18401d965ba85
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-03-06 09:27:49 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+const url = `http://localhost:3000/users`;
+
+function submitData(name, email) {
+
+    let configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+    return fetch(url, configurationObject)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object.id;
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message;
+    })
+}
